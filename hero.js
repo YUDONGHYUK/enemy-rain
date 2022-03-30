093429f9c310472b6639f5acae89f766ad836b0d
doyu <ydh0905@nate.com>
2022-03-30 17:16:53 +0900

    JS: change hero direction

--- hero.js
+++ hero.js
@@ -1,3 +1,10 @@
-class Hero {
-  
-}
+class Hero {}
+
+const hero = document.querySelector(".hero");
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    hero.style.backgroundPosition = "-70px";
+  } else if (e.key === "ArrowRight") {
+    hero.style.backgroundPosition = "-105px 0";
+  }
+});
